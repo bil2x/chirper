03ce4b52116a35930456706db14ec87e88e12fba
bil2x <bil2x33@gmail.com>
2024-12-16 14:19:10 +0530

    Inertia

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Chirp;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class ChirpController extends Controller
 {
@@ -12,7 +13,9 @@ class ChirpController extends Controller
      */
     public function index()
     {
-        return response('Hello World');
+        return Inertia::render('Chirps/Index',[
+            
+        ]);
     }
 
     /**
